a8952f8a99d40e9aac6388e192f47a7ca7950f0b
ryaneggz <kre8mymedia@gmail.com>
2025-08-03 17:02:06 -0500

    Work in progress

--- examples/langgraph_agent/src/langgraph_agent/main.py
+++ examples/langgraph_agent/src/langgraph_agent/main.py
@@ -2,7 +2,7 @@ import asyncio
 from dotenv import load_dotenv
 from graph import create_workflow
 from langchain_core.messages import HumanMessage
-from tools import get_weather, get_stock_price
+from tools import get_weather, get_stock_price, human_assistance
 from langchain.chat_models import init_chat_model
 from langchain.globals import set_llm_cache
 from langchain_openai import OpenAIEmbeddings
@@ -19,28 +19,32 @@ set_llm_cache(
     )
 )
 
-async def main(stream=True):
-    query = "TSLA stock price?"
+def main(stream=True):
+    # query = "TSLA stock price?"
+    query = "I need some expert guidance for building an AI agent. Could you request assistance for me?"
     workflow = create_workflow(
         llm=init_chat_model("openai:gpt-4.1-nano"),
-        tools=[get_weather, get_stock_price],
+        tools=[get_weather, get_stock_price, human_assistance],
         input_parser=input_parser
     )
     agent = workflow.compile()
-    if stream:
-        async for chunk in agent.astream({"messages": [HumanMessage(content=query)]}):
-            print(chunk)
-    else:
-        result = await agent.ainvoke({"messages": [HumanMessage(content=query)]})
-        print(result)
+    for event in agent.stream({"messages": [HumanMessage(content=query)]}, stream_mode='values'):
+        if "messages" in event:
+            event["messages"][-1].pretty_print()
+    # if stream:
+    #     async for chunk in agent.astream({"messages": [HumanMessage(content=query)]}):
+    #         print(chunk)
+    # else:
+    #     result = await agent.ainvoke({"messages": [HumanMessage(content=query)]})
+    #     print(result)
 
 
 if __name__ == "__main__":
     start_time = time()
-    asyncio.run(main())
+    main()
     end_time = time()
     print(f"Time taken: {end_time - start_time} seconds")
     start_time = time()
-    asyncio.run(main())
+    main()
     end_time = time()
     print(f"Time taken: {end_time - start_time} seconds (cached)")
--- examples/langgraph_agent/src/langgraph_agent/tools.py
+++ examples/langgraph_agent/src/langgraph_agent/tools.py
@@ -1,7 +1,7 @@
 import random
 
 from langchain_core.tools import tool
-
+from langgraph.types import Command, interrupt
 
 @tool(description="Get the weather in a given city")
 def get_weather(location: str) -> str:
@@ -10,3 +10,8 @@ def get_weather(location: str) -> str:
 @tool(description="Get the stock price of a given symbol")
 def get_stock_price(symbol: str) -> str:
     return f"The stock price of {symbol} is {random.randint(100, 1000)}"
+
+@tool(description="Request assistance from a human")
+def human_assistance(query: str) -> str:
+    human_response = interrupt({"query": query})
+    return human_response["data"]
